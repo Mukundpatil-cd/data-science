1550a9b2f59b02b6826f8ee094fbbb71a5693355
Mukundpatil-cd <mukundp2005@gmail.com>
2025-08-05 21:39:17 +0530

    modules and packages

---
+++ python/package/maths.py
@@ -0,0 +1,10 @@
+import random
+def addition(a,b):
+    return a+b
+
+def sub(c,d):
+    return c-d
+
+def get_random_number(start=1, end=100):
+    """Returns a random integer between start and end."""
+    return random.randint(start, end)    
---
+++ python/test.py
@@ -0,0 +1,12 @@
+from package.maths import *
+from package.sub.mult import multiply
+
+
+print(addition(34,54))
+print(sub(50,23))
+print(multiply(13,45))
+print(get_random_number())
+
+
+ 
+
