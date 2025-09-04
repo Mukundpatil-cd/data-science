3cd7543654261abe10d59f8aa623e27728e949e4
Mukund Mahesh Patil <Mukundp2005@gmail.com>
2025-09-04 21:23:50 +0530

    Add files via upload

---
+++ SQL/select.sql
@@ -0,0 +1,11 @@
+
+
+select * from Employees
+
+select firstname from Employees
+
+select firstname , lastname from Employees
+
+select EmployeeID , concat(firstname , ' ' , lastname) [full name] from Employees
+
+select EmployeeID , concat(firstname , ' ' , lastname) as [full name] from Employees
